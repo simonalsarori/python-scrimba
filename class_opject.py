a3c4324e06317879a547c439af46b41d31ea4a8c
Simon Al-Sarori <simon.alsarori@gmail.com>
2025-05-18 11:48:39 +0300

    Add zip and dictionary operations examples to zip.py

--- class_opject.py
+++ class_opject.py
@@ -22,3 +22,43 @@ films[0].nice_print()
 #Objects are the actual things you built
 #variables => attributes
 #functions => methods
+
+#################################################
+#inhertance
+
+class Person:
+    def move(self):
+        print("Moves 4 paces")
+    def rest(self):
+        print("Gains 4 health points")
+class Doctor(Person):
+    def heal(self):
+        print("Heals 10 health points")
+
+class Fighter(Person):
+    def fight(self):
+        print("Do 10 health points of damage")
+    def move(self):
+        print("Moves 6 paces")
+        
+class Wizard(Doctor,Fighter):
+    def cast_spell(self):
+        print("Turns invisble")
+    def heal(self):
+        print("Heals 15 health points")
+    
+    
+character1=Wizard()
+character1.move()
+
+
+#############################
+#model
+from platform import python_version as pv ,system
+
+print(pv())
+
+#or
+import platform, string, os
+
+print(platform.python_version())
